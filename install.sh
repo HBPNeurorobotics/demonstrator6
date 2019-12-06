fa05b19ca89537e9cc4fdb150af635171f265770
Jacques Kaiser <jkaiser@fzi.de>
2019-12-06 14:30:17 +0000

    fixed install script

--- install.sh
+++ install.sh
@@ -6,16 +6,10 @@
 sudo apt update
 sudo apt install ros-melodic-ros-control ros-melodic-ros-controllers ros-melodic-controller-manager ros-melodic-robot-state-publisher
 
-cp -r kuka_lbr_iiwa_generic_d6/ $NRP_MODELS_DIRECTORY
+cp -r ./Models/* $NRP_MODELS_DIRECTORY
 cd $NRP_MODELS_DIRECTORY
 ./create-symlinks.sh
 cd -
-
-cd $NRP_SOURCE_DIR/gzweb
-./deploy.sh -m local
-cd -
-# source $NRP_SOURCE_DIR/user-scripts/nrp_functions
-# build_gzweb
 
 source ~/.opt/platform_venv/bin/activate
 pip install torch
